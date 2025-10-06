f3abd3344687e3507ce49a5258ac1be05ae33132
JAM <jonas.muxel@student.htldornbirn.at>
2025-10-06 09:21:32 +0200

    added person api

--- cms_vue/src/App.vue
+++ cms_vue/src/App.vue
@@ -1,47 +1,57 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted } from "vue";
+import UserCard from "@/components/UserCard.vue";
+
+const people = ref([]);
+
+const api =
+  "https://fakerapi.it/api/v2/persons?_quantity=10&_locale=en_US&_seed=42";
+
+async function load() {
+  await fetch(api)
+    .then((response) => response.json())
+    .then((j) => {
+      people.value = (j.data || []).map((p, i) => ({
+        ...p,
+
+        image: `https://picsum.photos/1000/3000?rand=${Math.random().toString(
+          36
+        )}`,
+      }));
+    })
+    .catch((e) => console.error(e));
+}
+load();
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <h1>HTL Dornbirn 5bWI</h1>
+    <div class="container">
+      <UserCard
+        v-for="(person, index) in people"
+        :key="index"
+        :person="person"
+      ></UserCard>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+h1{
+  text-align: right;
+  font-size: 4rem;
+  font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  margin: 3rem 5rem 3rem 0rem;
+}
+.container {
+  align-items: center;
+  justify-content: center;
+  width: 80vw;
+  margin-left: 10vw;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+  gap: 24px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ cms_vue/src/components/UserCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="card">
+    <img class="card__img" :src="person.image" :alt="fullName" loading="lazy" />
+    <h2 class="card__name">{{ fullName }}</h2>
+    <p class="card__phone">{{ person.phone }}</p>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+const props = defineProps({
+  person: {
+    type: Object,
+    required: true,
+  },
+});
+
+const fullName = computed(() => {
+  const first = props.person.firstname ?? "";
+  const last = props.person.lastname ?? "";
+  return `${first} ${last}`.trim();
+});
+</script>
+
+<style scoped>
+.card {
+  background-color: rgba(54, 54, 54);
+  border: 2px rgb(73, 73, 73) solid;
+  border-radius: 10px;
+  padding: 5px;
+  transition-duration: 0.25s;
+}
+img {
+  width: 100%;
+  max-height: 400px;
+  object-fit: cover;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
+.card:hover {
+  transform: scale(1.05);
+  transform:perspective();
+  box-shadow: 0 0px 12px rgba(129, 163, 172, 0.35);
+  background-color: rgb(63, 73, 75);
+}
+</style>
